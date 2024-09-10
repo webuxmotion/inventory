6a45d67b0cec832ee67bc37bf5865f2e496bb902
webuxmotion <pereverziev.andrii@gmail.com>
2024-09-10 22:02:12 +0300

    Images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "s3-inventorymanagement-webuxmotion.s3.amazonaws.com",
         port: "",
         pathname: "/**",
       },
